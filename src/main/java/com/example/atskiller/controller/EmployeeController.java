5b1ac67a086437c6c81a351e02ad2f2566fd7e6c
nimashanishadi <protectyourprinter@gmail.com>
2025-05-10 11:53:18 -0400

    remove Employee Service

--- src/main/java/com/example/atskiller/controller/EmployeeController.java
+++ src/main/java/com/example/atskiller/controller/EmployeeController.java
@@ -1,35 +1,35 @@
-package com.example.atskiller.controller;
-
-import com.example.atskiller.model.Employee;
-import com.example.atskiller.service.EmployeeService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/employees")
-public class EmployeeController {
-
-
-    private final EmployeeService service;
-
-    public EmployeeController(EmployeeService service) {
-        this.service = service;
-    }
-
-
-    @GetMapping
-    public List<Employee> getEmployees() {
-        return service.getAllEmployees();
-    }
-
-    @PostMapping
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return service.saveEmployee(employee);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteEmployee(@PathVariable Long id) {
-        service.deleteEmployee(id);
-    }
-}
+//package com.example.atskiller.controller;
+//
+//import com.example.atskiller.model.Employee;
+//import com.example.atskiller.service.EmployeeService;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/employees")
+//public class EmployeeController {
+//
+//
+//    private final EmployeeService service;
+//
+//    public EmployeeController(EmployeeService service) {
+//        this.service = service;
+//    }
+//
+//
+//    @GetMapping
+//    public List<Employee> getEmployees() {
+//        return service.getAllEmployees();
+//    }
+//
+//    @PostMapping
+//    public Employee createEmployee(@RequestBody Employee employee) {
+//        return service.saveEmployee(employee);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteEmployee(@PathVariable Long id) {
+//        service.deleteEmployee(id);
+//    }
+//}
--- src/main/java/com/example/atskiller/service/EmployeeService.java
+++ src/main/java/com/example/atskiller/service/EmployeeService.java
@@ -1,29 +1,29 @@
-package com.example.atskiller.service;
-
-import com.example.atskiller.model.Employee;
-import com.example.atskiller.repository.EmployeeRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class EmployeeService {
-
-    private final EmployeeRepository repository;
-
-    public EmployeeService(EmployeeRepository repository) {
-        this.repository = repository;
-    }
-
-    public List<Employee> getAllEmployees() {
-        return repository.findAll();
-    }
-
-    public Employee saveEmployee(Employee employee) {
-        return repository.save(employee);
-    }
-
-    public void deleteEmployee(Long id) {
-        repository.deleteById(id);
-    }
-}
+//package com.example.atskiller.service;
+//
+//import com.example.atskiller.model.Employee;
+//import com.example.atskiller.repository.EmployeeRepository;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class EmployeeService {
+//
+//    private final EmployeeRepository repository;
+//
+//    public EmployeeService(EmployeeRepository repository) {
+//        this.repository = repository;
+//    }
+//
+//    public List<Employee> getAllEmployees() {
+//        return repository.findAll();
+//    }
+//
+//    public Employee saveEmployee(Employee employee) {
+//        return repository.save(employee);
+//    }
+//
+//    public void deleteEmployee(Long id) {
+//        repository.deleteById(id);
+//    }
+//}
